66e9c48187a230ae242e09acb705f63c824a8e22
mahiro-1 <riceomeletomuraisu@gmail.com>
2025-10-18 21:13:16 +0900

    fetch

--- restaurant-roulette/app/PackAnimation/page.tsx
+++ restaurant-roulette/app/PackAnimation/page.tsx
@@ -1,6 +1,12 @@
+import { HotpepperShop } from "@/types/HotpepperShop"
 import PackOpening from "../../components/animation/PackOpening"
+import { useEffect } from "react"
 
 export default function PackAnimation() {
+    useEffect(() => {
+        const data = await fetch(`api/hotpepper?lat=???`)
+        const shops = data.json() as HotpepperShop[];
+    }, [])
 
     return (
         <main className="min-h-screen bg-gradient-to-br from-primary/20 via-background to-accent/20">
